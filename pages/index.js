32b62460a696909eb34f3a135fe1c6c818fb19ab
jedidiahmarada <you@example.com>
2021-11-16 15:03:15 +0700

    duh

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+//our-domain.com/
+
+function Homepage(props) {
+  return (
+    <div>
+      <h1>The Home Page</h1>
+    </div>
+  );
+}
+
+export default Homepage;
---
+++ pages/news.js
@@ -0,0 +1,11 @@
+//our-domain.com/news
+
+function Newspage(props) {
+  return (
+    <div>
+      <h1>The News Page</h1>
+    </div>
+  );
+}
+
+export default Newspage;
